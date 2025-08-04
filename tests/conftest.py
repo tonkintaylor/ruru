6611c90721ebcaf150af4301638abc8dcbc7f490
Harel Lustiger <HLustiger@tonkintaylor.co.nz>
2025-08-04 16:17:20 +1200

    Refactor test configuration to simplify test paths and remove unnecessary ignores

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,15 +2,11 @@ from pathlib import Path
 
 import pytest
 
-ROOT_PATH = Path(__file__).parent.parent
-
 collect_ignore_glob = ["assets/**"]
-# Ignore doctests from cli.py module
-collect_ignore = [str(ROOT_PATH / "src" / "ruru" / "cli.py")]
 pytest_plugins = []
 
 
 @pytest.fixture(scope="session")
 def assets_dir() -> Path:
     """Return a path to the test assets directory."""
-    return ROOT_PATH / "tests" / "assets"
+    return Path(__file__).parent.parent / "tests" / "assets"
