381f4992f934275cb69f726c3b52adb8eff53a84
Nathan McDougall <nmcdougall@tonkintaylor.co.nz>
2026-03-20 12:08:17 +1300

    Simplify import statement

--- src/ruru/config.py
+++ src/ruru/config.py
@@ -5,13 +5,9 @@ Inspired by the R package `config` (https://rstudio.github.io/config/).
 
 import os
 import re
+from importlib.abc import Traversable
 from pathlib import Path
 from typing import Any, assert_never, overload
-
-try:
-    from importlib.resources.abc import Traversable
-except ModuleNotFoundError:
-    from importlib.abc import Traversable  # type: ignore[attr-defined]
 
 import yaml
 
